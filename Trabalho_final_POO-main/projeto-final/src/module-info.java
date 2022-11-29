fc7f3c1cf4c97715d10b8745086b703695bfdf4d
Lucasilva-tg <101115192+Lucasilva-tg@users.noreply.github.com>
2022-11-28 21:13:25 -0300

    Add files via upload

---
+++ Trabalho_final_POO-main/projeto-final/src/module-info.java
@@ -0,0 +1,10 @@
+module TrabalhoFinal {
+	exports br.com.newchatproject.controllers;
+	exports br.com.newchatproject.model;
+
+	requires java.sql;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+}
